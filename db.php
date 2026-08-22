e986b8d791006ac9cd1452819d3fb9e62507ede4
chtesportsofficial <cht.esports.official@gmail.com>
2026-08-22 14:54:25 +0600

    Update db.php

--- db.php
+++ db.php
@@ -4,8 +4,9 @@ $host = getenv('DB_HOST') ?: 'sql312.infinityfree.com';
 $user = getenv('DB_USER') ?: 'if0_42695077';
 $password = getenv('DB_PASSWORD') ?: 'h2jEoDfBkjiI';
 $dbname = getenv('DB_NAME') ?: 'if0_42695077_cht';
+$port = getenv('DB_PORT') ?: 3306;
 
-$conn = new mysqli($host, $user, $password, $dbname);
+$conn = new mysqli($host, $user, $password, $dbname, (int)$port);
 
 if ($conn->connect_error) {
     die("Database connection failed: " . $conn->connect_error);
